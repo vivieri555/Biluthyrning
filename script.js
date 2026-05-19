4f7226b263df479f2fad99f08346eb7c733e1103
unknown <vivieri555@gmail.com>
2026-05-19 11:46:58 +0200

    changes displays

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let loggInAttempts = 0;
 const password = "user";
 const username = "user";
 
+const basicAuth = btoa(`${username}:${password}`);
+
+let headers = new Headers(); 
+headers.append('Access-Control-Allow-Origin', 'http://localhost:8080'); 
+headers.append('Access-Control-Allow-Methods', 'GET'); 
+headers.append('Access-Control-Allow-Headers', 'Authorization'); 
+headers.append('Authorization', `Basic ${basicAuth}`); 
+
 loggInForm.addEventListener("submit", function(event) {
     event.preventDefault();
     validationLoggIn.textContent = "";
@@ -30,3 +38,30 @@ loggInForm.addEventListener("submit", function(event) {
         
     }
 });
+
+function logEvent(message) {
+    const timestamp = new Date().toLocaleString();
+    console.log(`[${timestamp}] ${message}`);
+}
+
+//Måste hämta data från backend för att kunna logga in, så denna funktion är inte klar än.
+
+function getLogIn() {
+    const url = "http://127.0.0.1:5500/v1/auth/login";
+    fetch(url,
+        {
+            method: "GET",
+            headers: headers
+        }
+    ) .then((response) => {
+        console.log(response.status);
+        if (!response.ok) throw new Error("Något gick fel med valideringen");
+        return response.json();
+    })
+    .then(data => {
+        validationLoggIn.textContent = data[0].firstName;
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    })
+}
